5eec7a5e75ed3c0d9f867a2f1a8f4b3f3d526730
Hero <16040582@vnu.edu.vn>
2022-08-03 01:48:51 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+const server = jsonServer.create();
+const router = jsonServer.router("api.json");
+const middlewares = jsonServer.defaults({
+  noCors: false,
+});
+
+server.db = router.db;
+
+const rules = auth.rewriter({
+  // Permission rules
+  // users: 600,
+  // messages: 640
+});
+
+// You must apply the middlewares in the following order
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
